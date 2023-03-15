5fb29fd9bcd5603d85cd41781a0cfaccc957fa25
Eduardo Uribe <eduardo.u24@gmail.com>
2023-03-15 10:07:18 -0300

    Added default classes

--- Assets/OpenRPG/Scripts/Data/CharacterClassData.cs
+++ Assets/OpenRPG/Scripts/Data/CharacterClassData.cs
@@ -1,10 +1,22 @@
-﻿using UnityEngine;
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using UnityEngine;
 
 namespace OpenRPG
 {
     [CreateAssetMenu(fileName = nameof(CharacterClassData), menuName = kBaseScriptableDataPath + nameof(CharacterClassData))]
     public class CharacterClassData : ScriptableData
     {
+        public override void ParseSource(string json)
+        {
+            JObject charClass = JsonConvert.DeserializeObject<JObject>(json);
 
+            if (charClass == null)
+                return;
+
+            string title = charClass.Value<string>("name");
+            
+            Title = title;
+        }
     }
 }
