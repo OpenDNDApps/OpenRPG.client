be2210bd5a3a5bed3c5b9f18728662577966d1f3
Eduardo Uribe <eduardo.u24@gmail.com>
2023-03-15 09:03:41 -0300

    Added default languages

--- Assets/OpenRPG/Scripts/Data/LanguageData.cs
+++ Assets/OpenRPG/Scripts/Data/LanguageData.cs
@@ -1,10 +1,22 @@
-﻿using UnityEngine;
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using UnityEngine;
 
 namespace OpenRPG
 {
     [CreateAssetMenu(fileName = nameof(LanguageData), menuName = kBaseScriptableDataPath + nameof(LanguageData))]
     public class LanguageData : ScriptableData
     {
+        public override void ParseSource(string json)
+        {
+            JObject language = JsonConvert.DeserializeObject<JObject>(json);
 
+            if (language == null)
+                return;
+
+            string title = language.Value<string>("name");
+            
+            Title = title;
+        }
     }
 }
